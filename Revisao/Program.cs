647a9b730acb39b3ca8531cb22a940861c4562c7
LuanSR <luan.sribeiro2016@gmail.com>
2022-03-21 22:05:52 -0300

    enviando exercícios 5 e 6

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -9,10 +9,13 @@ namespace Revisao
             Solucao ProvaRev = new Solucao();
 
             Console.WriteLine("Exercícios de Revisão");
+
             ProvaRev.Ex1();
             ProvaRev.Ex2();
             ProvaRev.Ex3();
-            
+            ProvaRev.Ex4();
+            ProvaRev.Ex5();
+            ProvaRev.Ex6();
         }
     }
 }
--- Revisao/Solucao.cs
+++ Revisao/Solucao.cs
@@ -8,16 +8,18 @@ namespace Revisao
     {
         public void Ex1()
         {
-                int num1, num2, soma;
+            Console.WriteLine("Exercício 1\n");
+            int num1, num2, soma;
 
-                Console.WriteLine("Digite dois numeros inteiros: \n");
-                num1 = int.Parse(Console.ReadLine());
-                num2 = int.Parse(Console.ReadLine());
-                soma = num1 + num2;
-                Console.WriteLine("A soma dos numero e igual a: {0}", soma);
+            Console.WriteLine("Digite dois numeros inteiros: \n");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            soma = num1 + num2;
+            Console.WriteLine("A soma dos numero e igual a: {0}", soma);
         }
         public void Ex2()
         {
+            Console.WriteLine("Exercício 2\n");
             double raio, area, pi = 3.14159;
 
             Console.WriteLine("Digite o valor do raio de um circulo:\n");
@@ -27,6 +29,7 @@ namespace Revisao
         }
         public void Ex3()
         {
+            Console.WriteLine("Exercício 3\n");
             int A, B, C, D, diferenca;
 
             Console.WriteLine("Digite quatro valores inteiros: ");
@@ -37,8 +40,9 @@ namespace Revisao
             diferenca = ((A * B) - (C * D));
             Console.WriteLine("A diferença do produto de A e B pelo produto de C e D e de {0}", diferenca);
         }
-        public void Ex4(
+        public void Ex4()
         {
+            Console.WriteLine("Exercício 4\n");
             int numf;
             float numh, valorh, sal;
 
@@ -51,7 +55,70 @@ namespace Revisao
             sal = numh * valorh;
             Console.WriteLine("\nNumero: {0}\n\nSalario: R${1:F2}\n", numf, sal);
         }
-        
+        public void Ex5()
+        {
+            Console.WriteLine("Exercício 5\n");
+            int cp1, qp1;
+            float pu1;
+            int cp2, qp2;
+            float pu2;
+            float vlrPagar;
+
+            Console.WriteLine("Peça 1");
+            cp1 = int.Parse(Console.ReadLine());
+            qp1 = int.Parse(Console.ReadLine());
+            pu1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Peça 2");
+            cp2 = int.Parse(Console.ReadLine());
+            qp2 = int.Parse(Console.ReadLine());
+            pu2 = int.Parse(Console.ReadLine());
+
+            vlrPagar = (qp1 * pu1) + (qp2 * pu2);
+            Console.WriteLine("Valor a ser pago: {0:F2}", vlrPagar);
+        }
+        public void Ex6()
+        {
+            float triangulo(float n1, float n2)
+            {
+                return (n1 * n2) / 2;
+            }
+            double circulo(float n1)
+            {
+                return 3.14159 * (n1 * n1);
+            }
+            float trapezio(float n1, float n2, float n3)
+            {
+                return ((n1 + n2) * n3) / 2;
+            }
+            float quadrado(float n1)
+            {
+                return n1 * n1;
+            }
+            float retangulo(float n1, float n2)
+            {
+                return n1 * n2;
+            }
+
+            {
+                float A, B, C, areatri, areac, areatra, areaq, arear;
+
+                Console.WriteLine("Digite tres numeros: \n\n");
+                A = float.Parse(Console.ReadLine());
+                B = float.Parse(Console.ReadLine());
+                C = float.Parse(Console.ReadLine());
+                areatri = triangulo(A, C);
+                Console.WriteLine("\nTriangulo: {0:F3}\n", areatri);
+                areac = (float)circulo(C);
+                Console.WriteLine("Circulo: {0:F3}\n", areac);
+                areatra = trapezio(A, B, C);
+                Console.WriteLine("Trapezio: {0:F3}\n", areatra);
+                areaq = quadrado(B);
+                Console.WriteLine("Quadrado: {0:F3}\n", areaq);
+                arear = retangulo(A, B);
+                Console.WriteLine("Retangulo: {0:F3}\n", arear);
+            }
+        }
     }
 }
 
